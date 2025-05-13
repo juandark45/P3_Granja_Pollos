dd4b683438a6212633b9797e4df0207833fd64e9
juandark45 <152802924+juandark45@users.noreply.github.com>
2025-05-12 23:00:31 -0500

    Update interfase.py

--- interfase.py
+++ interfase.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+class ICrud(ABC):
+    @abstractmethod
+    def crear_pollo(self, pollo): pass
+
+    @abstractmethod
+    def leer_pollo(self, codigo): pass
+
+    @abstractmethod
+    def actualizar_pollo(self, codigo, **kwargs): pass
+
+    @abstractmethod
+    def eliminar_pollo(self, codigo): pass
